f20a7499327d7c29a6a670b218c8233ec67ae6b6
UshnaMaheen84 <ushnamaheen84@gmail.com>
2020-10-09 11:54:49 +0500

    initial commit 9/10/2020

--- app/src/main/java/com/example/property/MainActivity.java
+++ app/src/main/java/com/example/property/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.property;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button logout;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logoutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                startActivity(new Intent(MainActivity.this, Login.class));
+                finish();
+
+            }
+        });
+
     }
 }
