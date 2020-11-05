7215eae864fa4609118c6f2b3dad633dd9e82256
UshnaMaheen84 <ushnamaheen84@gmail.com>
2020-11-05 11:29:46 +0500

    initial commit 5/11/2020

---
+++ app/src/main/java/com/example/property/ViewMap.java
@@ -0,0 +1,49 @@
+package com.example.property;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ViewMap extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    String lat , lng, name;
+    Float latitude, longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Intent intent = getIntent();
+
+        latitude = getIntent().getExtras().getFloat("lat");
+        longitude = getIntent().getExtras().getFloat("lng");
+        name = intent.getStringExtra("name");
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(name));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+
+    }
+}
