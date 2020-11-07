de0b8546fd2a20e6ab6c2a417ae9041f4bd83e03
bushrakamal96 <sweetybushra10@hotmail.com>
2020-11-07 12:10:08 +0500

    7/nov/2020 initial commit layout imguri

--- app/src/main/java/com/example/property/Add_Property.java
+++ app/src/main/java/com/example/property/Add_Property.java
@@ -1,9 +1,12 @@
 package com.example.property;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.app.AlertDialog;
@@ -14,13 +17,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.OpenableColumns;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -30,6 +36,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -37,6 +44,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.property.Adapter.UploadListAdapter;
 import com.example.property.models.AgentsModel;
 import com.example.property.models.Plots;
 import com.example.property.models.Precinct;
@@ -61,6 +69,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 import java.util.ArrayList;
@@ -108,6 +119,23 @@ public class Add_Property extends AppCompatActivity implements OnMapReadyCallbac
     double lat, lng;
     Button add_btn;
 
+
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private ImageButton mSelectBtn;
+    private RecyclerView mUploadList;
+
+    Uri fileUri;
+    String fileName;
+    private List<String> fileNameList;
+    private List<String> fileDoneList;
+
+    private UploadListAdapter uploadListAdapter;
+    private StorageReference mStorage;
+
+
+
+
     @Override
     public void onBackPressed() {
 
@@ -130,6 +158,9 @@ public class Add_Property extends AppCompatActivity implements OnMapReadyCallbac
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Please Wait...");
         progressDialog.setCanceledOnTouchOutside(false);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
 
         uId();
 
@@ -155,6 +186,32 @@ public class Add_Property extends AppCompatActivity implements OnMapReadyCallbac
                     startActivity(intent);
                     finish();
                 }
+            }
+        });
+
+        //image upload work
+
+        fileNameList = new ArrayList<>();
+        fileDoneList = new ArrayList<>();
+
+        uploadListAdapter = new UploadListAdapter(fileNameList, fileDoneList);
+
+        //   RecyclerView
+
+        mUploadList.setLayoutManager(new LinearLayoutManager(this));
+        mUploadList.setHasFixedSize(true);
+        mUploadList.setAdapter(uploadListAdapter);
+
+        mSelectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), RESULT_LOAD_IMAGE);
+
             }
         });
 
@@ -497,7 +554,7 @@ public class Add_Property extends AppCompatActivity implements OnMapReadyCallbac
                             && !plot_name.getText().toString().isEmpty()) {
 
                         Plots plots = new Plots(precinct_id, prprty_type_id, roadid, plotName, latitude, longitude, plotSq_yrd, plotRoom, plotStories, companyId,
-                                plotId, constructed, "No", agentId, agentName, price_from, price_to);
+                                plotId, constructed, "No", agentId, agentName, price_from, price_to, fileName);
 
                         databaseReference = FirebaseDatabase.getInstance().getReference().child("Plots");
 
@@ -583,6 +640,9 @@ public class Add_Property extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void uId() {
+
+        mSelectBtn = findViewById(R.id.imgBtn);
+        mUploadList = findViewById(R.id.imageRecyclerview);
 
 
         mainLayout = findViewById(R.id.main_layout1);
@@ -705,4 +765,74 @@ public class Add_Property extends AppCompatActivity implements OnMapReadyCallbac
                 break;
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
+
+            if(data.getClipData() != null) {
+
+                int totalItemsSelected = data.getClipData().getItemCount();
+
+                for(int i = 0; i < totalItemsSelected; i++){
+
+                     fileUri = data.getClipData().getItemAt(i).getUri();
+
+                     fileName = getFileName(fileUri);
+
+                    fileNameList.add(fileName);
+                    fileDoneList.add("uploading");
+                    uploadListAdapter.notifyDataSetChanged();
+                    StorageReference fileToUpload = mStorage.child("Images").child(fileName);
+
+                    final int finalI = i;
+                    fileToUpload.putFile(fileUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            fileDoneList.remove(finalI);
+                            fileDoneList.add(finalI, "done");
+
+                            uploadListAdapter.notifyDataSetChanged();
+
+                        }
+                    });
+
+                }
+
+            }  else if (data.getData() != null){
+
+                Toast.makeText(Add_Property.this, "Selected Single File", Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+
+    }
+
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+
 }
