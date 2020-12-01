01728962f38d893f46f5ab2cc300f65df79a24a0
bushrakamal96 <sweetybushra10@hotmail.com>
2020-12-01 19:14:57 +0500

    1/12/2020

---
+++ app/src/main/java/com/example/property/Sold_Property.java
@@ -0,0 +1,14 @@
+package com.example.property;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sold_Property extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sold__property_activity);
+    }
+}
