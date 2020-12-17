d828cddccb53c5700ce794f03f6348db6810f317
UshnaMaheen84 <ushnamaheen84@gmail.com>
2020-12-17 13:06:41 +0500

    initial commit 16/12/2020

--- app/src/main/java/com/example/property/Splash.java
+++ app/src/main/java/com/example/property/Splash.java
@@ -10,49 +10,35 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Splash extends AppCompatActivity {
 
-        FirebaseAuth firebaseAuth;
-        FirebaseUser currentUser;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser currentUser;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_splash);
-
-
-            firebaseAuth = FirebaseAuth.getInstance();
-            currentUser = firebaseAuth.getCurrentUser();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
 
-        }
-
-        @Override
-        protected void onStart() {
-            super.onStart();
-
-            if (currentUser == null) {
-
-                startActivity(new Intent(Splash.this, Login.class));
-                finish();
-            } else {
-                startActivity(new Intent(Splash.this, Dashboard.class));
-                finish();
-            }
-        }
+        firebaseAuth = FirebaseAuth.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
 
 
     }
 
-//            holder.mShare.setOnClickListener(new View.OnClickListener() {
-//@Override
-//public void onClick(View view) {
-//
-//        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-//        sharingIntent.setType("text/bold");
-//        sharingIntent.putExtra(Intent.EXTRA_TEXT, "Plot Name: " + model.getName()
-//        + "\nPlot No: " + model.getplot_no()
-//        + "\nRoad: " + model.getRoad_id()
-//        + "\nSq/yrd: " + model.getSq_yrds()
-//        + "\nPrice: Rs." + model.getPlot_price_range_to() + " to Rs." + model.getPlot_price_range_from());
-//        context.startActivity(Intent.createChooser(sharingIntent, "Share via"));
-//        }
-//        });
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser == null) {
+
+            startActivity(new Intent(Splash.this, Login.class));
+            finish();
+        } else {
+            startActivity(new Intent(Splash.this, Dashboard.class));
+            finish();
+        }
+    }
+
+
+}
+
